bcf43750553955c1c5267e01d1c03bb9a9eaefd4
artemternovenko <artem.inkaz@gmail.com>
2021-02-14 17:08:56 +0600

    [ADDED] item selected

--- app/src/main/java/com/example/sidebar/MainActivity.kt
+++ app/src/main/java/com/example/sidebar/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.sidebar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main2.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        nav_view.setNavigationItemSelectedListener (this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.id_account_menu -> Toast.makeText(this,"id_account_menu",Toast.LENGTH_SHORT).show()
+            R.id.location_menu -> Toast.makeText(this,"location_menu",Toast.LENGTH_SHORT).show()
+            R.id.tools_menu -> Toast.makeText(this,"tools_menu",Toast.LENGTH_SHORT).show()
+            R.id.tour_menu -> Toast.makeText(this,"tour_menu",Toast.LENGTH_SHORT).show()
+            R.id.useful_materials_menu -> Toast.makeText(this,"useful_materials_menu",Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }
